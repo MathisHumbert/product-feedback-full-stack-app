9898ad9b53005f0f73138c93ab803dbd400be2a3
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-11-13 09:32:53 +0100

    mobile css done

--- public/js/utils/createFeedback.js
+++ public/js/utils/createFeedback.js
@@ -1,8 +1,28 @@
 const createFeedback = (feedbacks) => {
-  return feedbacks
-    .map((feedback) => {
-      let { _id, title, category, upvotes, description, comments } = feedback;
-      return `
+  if (feedbacks.length === 0) {
+    const noFeedback = `
+    <div class="no-feedback">
+  <div class="no-feedback-container">
+    <img src="../assets/suggestions/illustration-empty.svg" alt="no-feedback" />
+    <h3>There is no feedback yet</h3>
+    <p class="body2">
+      Got a suggestion? Found a bug that needs to be squashed? We love hearing
+      about new ideas to improve our app.
+    </p>
+    <a href="feedback-new.html"
+      ><button class="button1">
+        + Add Feedback
+      </button></a
+    >
+  </div>
+</div>
+    `;
+    return noFeedback;
+  } else
+    return feedbacks
+      .map((feedback) => {
+        let { _id, title, category, upvotes, description, comments } = feedback;
+        return `
       <div class="single-feedback">
           <a href="feedback-detail.html?id=${_id}">${title}</a>
           <p class="body1">${description}</p>
@@ -19,8 +39,8 @@ const createFeedback = (feedbacks) => {
           </div>
         </div>
       `;
-    })
-    .join('');
+      })
+      .join('');
 };
 
 export default createFeedback;
