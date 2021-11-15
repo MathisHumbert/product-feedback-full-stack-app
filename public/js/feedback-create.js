a915c152159d512c76c9b1f4470ff09b35854003
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-11-15 08:58:46 +0100

    hmtl, css and js changed

---
+++ public/js/feedback-create.js
@@ -0,0 +1,8 @@
+// css
+// button size
+// button grid on new
+// add same style for both
+
+// module for opening and closing on the click class
+// module for cliking on the selected value class
+// patch axios
