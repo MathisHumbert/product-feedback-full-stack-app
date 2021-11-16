41ca6d5d05e8e97c80aae96727d4ba783a368011
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-11-16 18:04:24 +0100

    js changes

--- public/js/feedback-detail.js
+++ public/js/feedback-detail.js
@@ -1,3 +1,49 @@
+import createFeedback from './utils/createFeedback.js';
+
+const feedbackContainer = document.querySelector('.single-feedback-container');
+const allComments = document.querySelectorAll('.all-comments');
 const params = window.location.search;
 const id = new URLSearchParams(params).get('id');
-console.log(id);
+
+const getFeedback = async () => {
+  try {
+    const { data } = await axios.get(`/api/v1/feedbacks/${id}`);
+    feedbackContainer.innerHTML = createFeedback([data]);
+    displayAllComments(data.comments);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getFeedback();
+
+function displayAllComments(comments) {
+  comments.forEach((comment) => {
+    console.log(comment);
+    console.log(comment.user);
+
+    const commentEl = document.createElement('article');
+    commentEl.className = 'main-comment';
+    commentEl.innerHTML = `
+    <div class="main-comment-container">
+        <div class="header">
+          <div class="info">
+            <img src="../assets/user-images/image-anne.jpg" alt="">
+            <div class="text">
+              <h4>Elijah Moss</h4>
+              <p>@hexagon.bestagon</p>
+            </div>
+          </div>
+          <span class="reply-btn">Reply</span>
+        </div>
+        <div class="main-text">
+          Also, please allow styles to be applied based on system preferences. I would love to be able to browse Frontend Mentor in the evening after my device’s dark mode turns on without the bright background it currently has.
+        </div>
+        <form action="#" class="reply-form">
+          <textarea class="reply-input" required placeholder="Type your comment here"></textarea>
+          <input type="submit" value="Post Reply" class="button1 reply-btn">
+        </form>
+        </div>
+    `;
+  });
+}
